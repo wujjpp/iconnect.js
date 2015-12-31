5ae7d4a1fe39911a87d7079841b4493f12afcd56
Wu Jian Ping <wujjpp@hotmail.com>
2015-12-31 13:21:15 +0800

    Add render content in sub-page's container and add active class for tab

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -111,272 +111,37 @@ angular.module('iconnectApp').config(['$stateProvider', '$urlRouterProvider', '$
       controller: 'OrderEditorCtrl'
     })
 
-    .state('authorised.subrouters', {
-      url: '/subrouters',
+    .state('authorised.subrouters',{
+      url:'/subrouters',
       templateUrl: 'views/sub-routers/index.html',
       controller: 'SubRouterMainCtrl'
-    }).state('authorised.subrouters.page1', {
-      url: '/page1',
+    }).state('authorised.subrouters.page1',{
+      url:'/page1',
       //templateUrl: 'views/sample-product.html',
       //controller: 'ProductCtrl'
       templateUrl: 'views/sample-customer.html',
       controller: 'CustomerCtrl'
       //templateUrl: 'views/sub-routers/page-1.html',
       //controller: 'SubRouterPage1Ctrl'
-    }).state('authorised.subrouters.page2', {
-      url: '/page2',
+    }).state('authorised.subrouters.page2',{
+      url:'/page2',
       templateUrl: 'views/todo.html',
       controller: 'ToDoCtrl'
       //templateUrl: 'views/sub-routers/page-2.html',
       //controller: 'SubRouterPage2Ctrl'
-    }).state('authorised.subrouters.page3', {
-      url: '/page3',
+    }).state('authorised.subrouters.page3',{
+      url:'/page3',
       templateUrl: 'views/sub-routers/page-3.html',
       controller: 'SubRouterPage3Ctrl'
     }).state('authorised.subrouters.create', {
-      url: '/create',
+      url:'/create',
       templateUrl: 'views/sub-routers/page-3-create.html',
       controller: 'SubRouterPage3CreateCtrl'
     }).state('authorised.subrouters.page3.create', {
-      url: '/create',
+      url:'/create',
       templateUrl: 'views/sub-routers/page-3-create.html',
       controller: 'SubRouterPage3CreateCtrl'
-    })
+    });;
 
-    //performance testing
-    .state('authorised.static-local-records-100', {
-      url: '/static-local-records-100',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 100
-    })
-    .state('authorised.dynamic-local-records-100', {
-      url: '/dynamic-local-records-100',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 100
-    })
-    .state('authorised.static-local-records-200', {
-      url: '/static-local-records-200',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 200
-    })
-    .state('authorised.dynamic-local-records-200', {
-      url: '/dynamic-local-records-200',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 200
-    })
-    .state('authorised.static-local-records-500', {
-      url: '/static-local-records-500',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 500
-    })
-    .state('authorised.dynamic-local-records-500', {
-      url: '/dynamic-local-records-500',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 500
-    })
-    .state('authorised.static-local-records-1000', {
-      url: '/static-local-records-1000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 1000
-    })
-    .state('authorised.dynamic-local-records-1000', {
-      url: '/dynamic-local-records-1000',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 1000
-    })
-    .state('authorised.static-local-records-1500', {
-      url: '/static-local-records-1500',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 1500
-    })
-    .state('authorised.dynamic-local-records-1500', {
-      url: '/dynamic-local-records-1500',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 1500
-    })
-    .state('authorised.static-local-records-2000', {
-      url: '/static-local-records-2000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 2000
-    })
-    .state('authorised.static-local-records-3000', {
-      url: '/static-local-records-3000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 3000
-    })
-    .state('authorised.static-local-records-4000', {
-      url: '/static-local-records-4000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 4000
-    })
-    .state('authorised.static-local-records-5000', {
-      url: '/static-local-records-5000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceLocalCtrl',
-      count: 5000
-    })
-
-    .state('authorised.static-remote-records-100', {
-      url: '/static-remote-records-100',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 100
-    })
-    .state('authorised.dynamic-remote-records-100', {
-      url: '/dynamic-remote-records-100',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 100
-    })
-    .state('authorised.static-remote-records-200', {
-      url: '/static-remote-records-200',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 200
-    })
-    .state('authorised.dynamic-remote-records-200', {
-      url: '/dynamic-remote-records-200',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 200
-    })
-    .state('authorised.static-remote-records-500', {
-      url: '/static-remote-records-500',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 500
-    })
-    .state('authorised.dynamic-remote-records-500', {
-      url: '/dynamic-remote-records-500',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 500
-    })
-    .state('authorised.static-remote-records-1000', {
-      url: '/static-remote-records-1000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 1000
-    })
-    .state('authorised.dynamic-remote-records-1000', {
-      url: '/dynamic-remote-records-1000',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 1000
-    })
-    .state('authorised.static-remote-records-1500', {
-      url: '/static-remote-records-1500',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 1500
-    })
-    .state('authorised.dynamic-remote-records-1500', {
-      url: '/dynamic-remote-records-1500',
-      templateUrl: 'views/sample-test-performance-dynamic.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 1500
-    })
-    .state('authorised.static-remote-records-2000', {
-      url: '/static-remote-records-2000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 2000
-    })
-    .state('authorised.static-remote-records-3000', {
-      url: '/static-remote-records-3000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 3000
-    })
-    .state('authorised.static-remote-records-4000', {
-      url: '/static-remote-records-4000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 4000
-    })
-    .state('authorised.static-remote-records-5000', {
-      url: '/static-remote-records-5000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 5000
-    })
-    .state('authorised.static-remote-records-10000', {
-      url: '/static-remote-records-10000',
-      templateUrl: 'views/sample-test-performance-static.html',
-      controller: 'SampleTestPerformanceRemoteCtrl',
-      count: 10000
-    })
-
-    .state('authorised.browser-capacity-dom-1500', {
-      url: '/browser-capacity-1500',
-      templateUrl: 'views/sample-test-performance-input.html',
-      controller: 'SampleTestPerformanceInputCtrl',
-      count: 1500
-    })
-    .state('authorised.browser-capacity-dom-2000', {
-      url: '/browser-capacity-2000',
-      templateUrl: 'views/sample-test-performance-input.html',
-      controller: 'SampleTestPerformanceInputCtrl',
-      count: 2000
-    })
-    .state('authorised.browser-capacity-dom-3000', {
-      url: '/browser-capacity-3000',
-      templateUrl: 'views/sample-test-performance-input.html',
-      controller: 'SampleTestPerformanceInputCtrl',
-      count: 3000
-    })
-    .state('authorised.browser-capacity-dom-4000', {
-      url: '/browser-capacity-4000',
-      templateUrl: 'views/sample-test-performance-input.html',
-      controller: 'SampleTestPerformanceInputCtrl',
-      count: 3000
-    })
-    .state('authorised.browser-capacity-dom-5000', {
-      url: '/browser-capacity-5000',
-      templateUrl: 'views/sample-test-performance-input.html',
-      controller: 'SampleTestPerformanceInputCtrl',
-      count: 3000
-    })
-    .state('authorised.browser-capacity-dom-10000', {
-      url: '/browser-capacity-10000',
-      templateUrl: 'views/sample-test-performance-input.html',
-      controller: 'SampleTestPerformanceInputCtrl',
-      count: 10000
-    })
-    .state('authorised.data-from-local', {
-      url: '/data-from-local/:count',
-      templateUrl: 'views/sample-test-performance.html',
-      controller: 'SampleTestPerformanceCtrl',
-      dataFrom: 'local'
-    })
-    .state('authorised.data-from-remote', {
-      url: '/data-from-remote/:count',
-      templateUrl: 'views/sample-test-performance.html',
-      controller: 'SampleTestPerformanceCtrl',
-      dataFrom: 'remote'
-    })
-    .state('authorised.test-dropdown-performance', {
-      url: '/test-dropdown-performance',
-      templateUrl: 'views/sample-test-performance-dropdown.html',
-      controller: 'SampleTestPerformanceDropdownCtrl'
-    })
-    .state('authorised.test-dropdown-performance-normal', {
-      url: '/test-dropdown-performance-normal',
-      templateUrl: 'views/sample-test-performance-dropdown-normal.html',
-      controller: 'SampleTestPerformanceDropdownCtrl'
-    });
 }]);
 
